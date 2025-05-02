8a7b32fa709e30057f051112c0d5c515d6b4abc5
Jonas@WinLenTP <jonas.addicks@gmx.de>
2025-05-02 17:07:37 +0200

    nit so sicher was das hier noch ist

--- src/main/java/game/GameProperties.java
+++ src/main/java/game/GameProperties.java
@@ -5,7 +5,7 @@ public class GameProperties {
     public static final int COLS = 7;
 
     //In case 5: BenchmarkAIvsPlayer the player has to start
-    public static final int GAMEMODE = 5; //Multiplayer: 0, Single player: 1, AIOnly: 2, Train: 3, BenchmarkAIvsAI: 4, BenchmarkAIvsPlayer: 5
+    public static final int GAMEMODE = 1; //Multiplayer: 0, Single player: 1, AIOnly: 2, Train: 3, BenchmarkAIvsAI: 4, BenchmarkAIvsPlayer: 5
     public static final boolean PLAYER1_STARTS = true;
 
     public static final int NR_BENCHMARK_GAMES = 500;
